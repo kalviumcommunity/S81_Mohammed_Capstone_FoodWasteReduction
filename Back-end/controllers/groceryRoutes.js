c26f15dc1064de73d99d2ea663f9796e48f58583
Mohammedshammas28 <m.shammas2k6@gmail.com>
2025-05-05 17:08:36 +0530

    Get

--- Back-end/controllers/groceryRoutes.js
+++ Back-end/controllers/groceryRoutes.js
@@ -28,17 +28,3 @@ groceryRouter.post('/add', async (req, res) => {
   }
 });
 
-// Get grocery items for a user
-groceryRouter.get('/:userId', async (req, res) => {
-  try {
-    const items = await Grocery.find({ userId: req.params.userId });
-    res.json(items);
-  } catch (err) {
-    res.status(500).json({ message: 'Error fetching groceries', error: err.message });
-  }
-});
-
-
-
-
- module.exports = groceryRouter;
