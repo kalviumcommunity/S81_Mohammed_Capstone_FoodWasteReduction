525fdbd2707425c31bb5f75bf30ed95ef5f3cc8b
Mohammedshammas28 <m.shammas2k6@gmail.com>
2025-05-05 17:09:42 +0530

    Get

--- Back-end/controllers/profileRoutes.js
+++ Back-end/controllers/profileRoutes.js
@@ -2,3 +2,16 @@ const express = require('express');
 const profileRouter = express.Router();
 const userModel = require('../model/userModel');
 
+// Get profile by userId
+profileRouter.get('/:userId', async (req, res) => {
+  try {
+    const user = await UserModel.findById(req.params.userId).select('-password');
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ message: "Error fetching user profile", error: err.message });
+  }
+});
+
+module.exports = profileRouter;
