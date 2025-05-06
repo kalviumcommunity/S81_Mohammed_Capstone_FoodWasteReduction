781be9287ad3012569150b11d05b831891cba20a
Mohammedshammas28 <m.shammas2k6@gmail.com>
2025-05-06 09:37:35 +0530

    post api

--- Back-end/controllers/groceryRoutes.js
+++ Back-end/controllers/groceryRoutes.js
@@ -3,7 +3,29 @@ const groceryRouter = express.Router();
 const GroceryModel = require('../model/groceryModel');
 const { estimateExpiry, getStorageTip } = require('../utils/expiryHelper');
 
+// Add a grocery item
+groceryRouter.post('/add', async (req, res) => {
+  const { userId, itemName, quantity, purchaseDate } = req.body;
 
+  try {
+    const estimatedExpiry = estimateExpiry(itemName, purchaseDate);
+    const storageTip = getStorageTip(itemName);
+
+    const item = new Grocery({
+      userId,
+      itemName,
+      quantity,
+      purchaseDate,
+      estimatedExpiry,
+      storageTip,
+    });
+
+    await item.save();
+    res.status(201).json(item);
+  } catch (err) {
+    res.status(500).json({ message: 'Error adding item', error: err.message });
+  }
+});
 
 // Get grocery items for a user
 groceryRouter.get('/:userId', async (req, res) => {
