2faeb6578c4089d9b020f3d90f7a2b9d7cf16494
Mohammedshammas28 <m.shammas2k6@gmail.com>
2025-05-29 11:16:12 +0530

    Done

--- Back-end/controllers/groceryRoutes.js
+++ Back-end/controllers/groceryRoutes.js
@@ -1,49 +1,51 @@
 const express = require('express');
-const groceryRouter = express.Router();
-const GroceryModel = require('../model/groceryModel');
-const { estimateExpiry, getStorageTip } = require('../utils/expiryHelper');
+  const groceryRouter = express.Router();
+  const GroceryModel = require('../model/groceryModel');
+  const { estimateExpiry, getStorageTip } = require('../utils/expiryHelper');
+  // const calculateExpiry = require('../utils/calculateExpiry');
 
-// Add a grocery item
-groceryRouter.post('/add', async (req, res) => {
-  const { userId, itemName, quantity, purchaseDate } = req.body;
+  // Add a grocery item
+  groceryRouter.post('/add', async (req, res) => {
+    const { user, name, quantity, purchaseDate } = req.body;
 
-  try {
-    const estimatedExpiry = estimateExpiry(itemName, purchaseDate);
-    const storageTip = getStorageTip(itemName);
+    try {
+      const estimatedExpiry = estimateExpiry(name, purchaseDate);
+      const storageTip = getStorageTip(name);
 
-    const item = new GroceryModel({
-      user: userId,
-      name: itemName,
-      quantity,
-      purchaseDate,
-      expiryDate: estimatedExpiry,
-      storageTips: storageTip,
-    });
+      const item = new GroceryModel({
+        user: user,
+        name: name,
+        quantity,
+        purchaseDate,
+        expiryDate: estimatedExpiry,
+        storageTips: storageTip,
+      });
 
-    await item.save();
-    res.status(201).json(item);
-  } catch (err) {
-    res.status(500).json({ message: 'Error adding item', error: err.message });
-  }
-});
+      await item.save();
+      res.status(201).json(item);
+    } catch (err) {
+      res.status(500).json({ message: 'Error adding item', error: err.message });
+    }
+  });
 
-// Get grocery items for a user
-groceryRouter.get('/user/:userId', async (req, res) => {
-  try {
-    const items = await GroceryModel.find({ user: req.params.userId });
-    res.json(items);
-  } catch (err) {
-    res.status(500).json({ message: 'Error fetching groceries', error: err.message });
-  }
-});
+  // Get grocery items for a user
+  groceryRouter.get('/:userId', async (req, res) => {
+    try {
+      const items = await Grocery.find({ userId: req.params.userId });
+      res.json(items);
+    } catch (err) {
+      res.status(500).json({ message: 'Error fetching groceries', error: err.message });
+    }
+  });
 
+  
   // Update grocery item by ID
   groceryRouter.put('/update/:id', async (req, res) => {
     try {
-      const { itemName, quantity, purchaseDate } = req.body;
+      const { name, quantity, purchaseDate } = req.body;
       const updated = await GroceryModel.findByIdAndUpdate(
         req.params.id,
-        { name: itemName, quantity, purchaseDate },
+        { name: name, quantity, purchaseDate },
         { new: true }
       );
 
--- Back-end/controllers/userRoutes.js
+++ Back-end/controllers/userRoutes.js
@@ -1,76 +1,70 @@
 const express = require("express");
-const { UserModel } = require("../model/userModel");
-const Errorhandler = require("../utils/errorhadler");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const catchAsyncError = require("../middleware/catchAsyncError");
-const userRouter = express.Router();
-require("dotenv").config();
+  const { UserModel } = require("../model/userModel");
+  const Errorhandler = require("../utils/errorhadler");
+  const bcrypt = require("bcrypt");
+  const jwt = require("jsonwebtoken");
+  const catchAsyncError = require("../middleware/catchAsyncError");
+  const userRouter = express.Router();
+  require("dotenv").config();
 
-userRouter.get("/signup", (req, res) => {
-res.status(200).send("Signup Page");
-});
+  // userRouter.get("/signup", (req, res) => {
+  //   res.status(200).send("Signup Page");
+  // });  
 
-userRouter.post("/signup",
-catchAsyncError(async (req, res, next) => {
-const { name, email, password } = req.body;
+  userRouter.post("/signup",
+    catchAsyncError(async (req, res, next) => {
+      const { name, email, password } = req.body;
 
+      if (!email || !name || !password) {
+        return next(new Errorhandler("All fields are required", 400));
+      }
 
-if (!email || !name || !password) {
-  return next(new Errorhandler("All fields are required", 400));
-}
+      if (!email.match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)) {
+        return next(new Errorhandler("Invalid email format", 400));
+      }
 
-if (!email.match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)) {
-  return next(new Errorhandler("Invalid email format", 400));
-}
+    
+      const userExists = await UserModel.findOne({ email });
+      if (userExists) {
+        return next(new Errorhandler("User already exists", 400));
+      }
 
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const newUser = await UserModel.create({ name, email, password: hashedPassword });
 
-const userExists = await UserModel.findOne({ email });
-if (userExists) {
-  return next(new Errorhandler("User already exists", 400));
-}
+      res.status(201).json({ success: true, message: "Signup successful", user: newUser });
+    })
+  );
 
-const hashedPassword = await bcrypt.hash(password, 10);
-const newUser = await UserModel.create({ name, email, password: hashedPassword });
+  userRouter.post("/login",
+    catchAsyncError(async (req, res, next) => {
+      const { email, password } = req.body;
 
-res.status(201).json({ success: true, message: "Signup successful", user: newUser });
+      if (!email || !password) {
+        return next(new Errorhandler("Email and password are required", 400));
+      }
 
+      const user = await UserModel.findOne({ email });
+      if (!user) {
+        return next(new Errorhandler("Incorrect login details", 400));
+      }
 
-})
-);
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return next(new Errorhandler("Incorrect login details", 400));
+      }
 
-userRouter.post("/login",
-catchAsyncError(async (req, res, next) => {
-const { email, password } = req.body;
+      const token = jwt.sign({ id: user._id }, process.env.SECRET, { expiresIn: "30d" });
+      res.cookie("accesstoken", token, {
+        httpOnly: true,
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+      });
 
+      res.status(200).json({ success: true, message: "Login successful" });
+    })
+  );
 
-if (!email || !password) {
-  return next(new Errorhandler("Email and password are required", 400));
-}
-
-const user = await UserModel.findOne({ email });
-if (!user) {
-  return next(new Errorhandler("Incorrect login details", 400));
-}
-
-const isMatch = await bcrypt.compare(password, user.password);
-if (!isMatch) {
-  return next(new Errorhandler("Incorrect login details", 400));
-}
-
-const token = jwt.sign({ id: user._id }, process.env.SECRET, { expiresIn: "30d" });
-res.cookie("accesstoken", token, {
-        // httpOnly: true,
-      secure: false,
-      sameSite: "lax",// Protects against CSRF
-  maxAge: 24 * 60 * 60 * 1000 // 1 day
-});
-
-res.status(200).json({ success: true, message: "Login successful", token });
-
-
-})
-);
+ 
 
 // userRouter.post("/forgotpassword",catchAsyncError(async (req, res, next) => {
 
