07202cda5982e00271fb1242318c1685f15fc32e
Mohammedshammas28 <m.shammas2k6@gmail.com>
2025-05-29 11:06:37 +0530

    Done

--- Back-end/controllers/groceryRoutes.js
+++ Back-end/controllers/groceryRoutes.js
@@ -1,41 +1,41 @@
-  const express = require('express');
-  const groceryRouter = express.Router();
-  const GroceryModel = require('../model/groceryModel');
-  const { estimateExpiry, getStorageTip } = require('../utils/expiryHelper');
-  // const calculateExpiry = require('../utils/calculateExpiry');
+    const express = require('express');
+    const groceryRouter = express.Router();
+    const GroceryModel = require('../model/groceryModel');
+    const { estimateExpiry, getStorageTip } = require('../utils/expiryHelper');
+    // const calculateExpiry = require('../utils/calculateExpiry');
 
-  // Add a grocery item
-  groceryRouter.post('/add', async (req, res) => {
-    const { user, name, quantity, purchaseDate } = req.body;
+    // Add a grocery item
+    groceryRouter.post('/add', async (req, res) => {
+      const { user, name, quantity, purchaseDate } = req.body;
 
-    try {
-      const estimatedExpiry = estimateExpiry(name, purchaseDate);
-      const storageTip = getStorageTip(name);
+      try {
+        const estimatedExpiry = estimateExpiry(name, purchaseDate);
+        const storageTip = getStorageTip(name);
 
-      const item = new GroceryModel({
-        user: user,
-        name: name,
-        quantity,
-        purchaseDate,
-        expiryDate: estimatedExpiry,
-        storageTips: storageTip,
-      });
+        const item = new GroceryModel({
+          user: user,
+          name: name,
+          quantity,
+          purchaseDate,
+          expiryDate: estimatedExpiry,
+          storageTips: storageTip,
+        });
 
-      await item.save();
-      res.status(201).json(item);
-    } catch (err) {
-      res.status(500).json({ message: 'Error adding item', error: err.message });
-    }
-  });
+        await item.save();
+        res.status(201).json(item);
+      } catch (err) {
+        res.status(500).json({ message: 'Error adding item', error: err.message });
+      }
+    });
 
-  // Get grocery items for a user
-  groceryRouter.get('/:userId', async (req, res) => {
-    try {
-      const items = await Grocery.find({ userId: req.params.userId });
-      res.json(items);
-    } catch (err) {
-      res.status(500).json({ message: 'Error fetching groceries', error: err.message });
-    }
-  });
+    // Get grocery items for a user
+    groceryRouter.get('/:userId', async (req, res) => {
+      try {
+        const items = await Grocery.find({ userId: req.params.userId });
+        res.json(items);
+      } catch (err) {
+        res.status(500).json({ message: 'Error fetching groceries', error: err.message });
+      }
+    });
 
-  module.exports = groceryRouter;
+    module.exports = groceryRouter;
