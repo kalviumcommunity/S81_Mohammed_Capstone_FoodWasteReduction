4aea797f2ccc02afdc45c8a0170af6b400f13795
Mohammedshammas28 <m.shammas2k6@gmail.com>
2025-05-29 10:55:50 +0530

    Done

--- Back-end/model/groceryModel.js
+++ Back-end/model/groceryModel.js
@@ -1,34 +1,34 @@
-const mongoose = require("mongoose");
+  const mongoose = require("mongoose");
 
-const grocerySchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  name: {
-    type: String,
-    required: true,
-  },
-  quantity: {
-    type: Number,
-    required: true,
-  },
-  purchaseDate: {
-    type: Date,
-    required: true,
-  },
-  expiryDate: {
-    type: Date,
-  },
-  storageTips: {
-    type: String,
-  },
-  isUsed: {
-    type: Boolean,
-    default: false,
-  },
-});
+  const grocerySchema = new mongoose.Schema({
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
+    purchaseDate: {
+      type: Date,
+      required: true,
+    },
+    expiryDate: {
+      type: Date,
+    },
+    storageTips: {
+      type: String,
+    },
+    isUsed: {
+      type: Boolean,
+      default: false,
+    },
+  });
 
-const GroceryModel = mongoose.model("grocery", grocerySchema);
-module.exports = GroceryModel;
+  const GroceryModel = mongoose.model("grocery", grocerySchema);
+  module.exports = GroceryModel;
--- Back-end/model/userModel.js
+++ Back-end/model/userModel.js
@@ -1,32 +1,32 @@
-const mongoose = require("mongoose");
+  const mongoose = require("mongoose");
 
-const userSchema =  mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 8,
-  },
-  language: {
-    type: String,
-    default: "English", 
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  const userSchema =  mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+    },
+    language: {
+      type: String,
+      default: "English", 
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  });
 
-UserModel = mongoose.model("User", userSchema);
+  UserModel = mongoose.model("User", userSchema);
 
-module.exports= {UserModel}
+  module.exports= {UserModel}
 
